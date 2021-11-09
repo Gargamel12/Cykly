15aa697ddfa66b3e3d086b48819adff02d801ef8
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2021-11-09 09:37:14 +0100

    Přidejte soubory projektu.

---
+++ Cykly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cykly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 2; i < 100; i+=2) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
